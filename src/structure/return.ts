a7afc44325bfe0a97d8a936b078946821d4903f7
wmxpy <wm@mengw.io>
2022-06-22 23:40:45 -0500

    add structure

---
+++ src/structure/return.ts
@@ -0,0 +1,39 @@
+/**
+ * @author WMXPY
+ * @namespace Structure
+ * @description Return
+ */
+
+export type SudoRPCReturn<SuccessResult, FailResult> =
+    | SudoRPCReturnV1<SuccessResult, FailResult>;
+
+export type SudoRPCReturnV1<SuccessResult, FailResult> =
+    SudoRPCReturnV1Base
+    & (
+        | SudoRPCReturnV1Success<SuccessResult>
+        | SudoRPCReturnV1Fail<FailResult>
+    );
+
+export type SudoRPCReturnV1Base = {
+
+    readonly version: "1.0";
+
+    readonly identifier: string;
+};
+
+export type SudoRPCReturnV1Success<SuccessResult> = {
+
+    readonly success: true;
+
+    readonly result: SuccessResult;
+};
+
+export type SudoRPCReturnV1Fail<FailResult> = {
+
+    readonly success: false;
+
+    readonly code: number;
+    readonly message: string;
+
+    readonly result: FailResult;
+};
---
+++ src/structure/sudorpc.ts
@@ -0,0 +1,19 @@
+/**
+ * @author WMXPY
+ * @namespace Structure
+ * @description SudoRPC
+ */
+
+export type SudoRPCCall<Metadata, Payload> =
+    | SudoRPCCallV1<Metadata, Payload>;
+
+export type SudoRPCCallV1<Metadata, Payload> = {
+
+    readonly version: "1.0";
+
+    readonly resource: string;
+    readonly identifier: string;
+
+    readonly metadata: Metadata;
+    readonly payload: Payload;
+};
