048510821851617a1c3a23b0af1527c726d2c3af
wmxpy <wm@mengw.io>
2022-06-23 00:20:25 -0500

    add return agent

---
+++ src/handler/agent.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace Handler
+ * @description Helper
+ */
+
+export class SudoRPCHandlerHelper {
+
+}
---
+++ src/handler/return.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace Handler
+ * @description Return
+ */
+
+import { SudoRPCReturn } from "../structure/return";
+
+export const createSuccessReturn = <SuccessResult>(result: SuccessResult): SudoRPCReturn<SuccessResult, any> => {
+
+    return null as any;
+};
