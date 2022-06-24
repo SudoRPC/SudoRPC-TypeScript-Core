00a8727356bee585f59a66c3c152f97e081a0d9a
wmxpy <wm@mengw.io>
2022-06-23 21:37:41 -0500

    add service register

--- src/resource/resource.ts
+++ src/resource/resource.ts
@@ -6,9 +6,37 @@
 
 export class SudoRPCResource {
 
+    private readonly _identifier: string;
     private readonly _dependencies: string[];
+    private readonly _satisfies: string[];
 
-    private constructor() {
+    private constructor(
+        identifier: string,
+        dependencies: string[] = [],
+        satisfies: string[] = [],
+    ) {
+        this._identifier = identifier;
+        this._dependencies = dependencies;
+        this._satisfies = satisfies;
+    }
 
+    public get identifier(): string {
+        return this._identifier;
+    }
+
+    public get dependencies(): string[] {
+        return this._dependencies;
+    }
+
+    public get satisfies(): string[] {
+        return this._satisfies;
+    }
+
+    public addDependency(dependency: string): void {
+        this._dependencies.push(dependency);
+    }
+
+    public addSatisfy(satisfy: string): void {
+        this._satisfies.push(satisfy);
     }
 }
--- src/service/service.ts
+++ src/service/service.ts
@@ -4,7 +4,28 @@
  * @description Service
  */
 
+import { SudoRPCResource } from "../resource/resource";
+
 export class SudoRPCService {
 
+    private readonly _resources: Set<SudoRPCResource>;
+    private readonly _satisfies: Map<string, WeakSet<SudoRPCResource>>;
 
+    private constructor() {
+
+        this._resources = new Set();
+        this._satisfies = new Map();
+    }
+
+    public register(resource: SudoRPCResource): void {
+
+        this._resources.add(resource);
+        for (const satisfy of resource.satisfies) {
+
+            if (!this._satisfies.has(satisfy)) {
+                this._satisfies.set(satisfy, new WeakSet());
+            }
+            this._satisfies.get(satisfy)!.add(resource);
+        }
+    }
 }
