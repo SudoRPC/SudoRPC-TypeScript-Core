f0f574dc46b197cdc7fac5ef160c8d187dace7e6
wmxpy <wm@mengw.io>
2022-06-26 12:46:56 -0500

    implement context

--- src/handler/context.ts
+++ src/handler/context.ts
@@ -29,4 +29,54 @@ export class SudoRPCHandlerContext<Metadata, Payload> {
 
         this._namespaces = new Map();
     }
+
+    public get metadata(): Metadata {
+        return this._call.metadata;
+    }
+
+    public get payload(): Payload {
+        return this._call.payload;
+    }
+
+    public setDefaultContext(key: string, value: any): this {
+
+        return this.setContext(DEFAULT_NAMESPACE_SYMBOL, key, value);
+    }
+
+    public setContext(namespace: symbol | string, key: string, value: any): this {
+
+        const contextMap = this._getNamespaceMap(namespace);
+        contextMap.set(key, value);
+        return this;
+    }
+
+    public getDefaultContext(key: string): any {
+
+        return this.getContext(DEFAULT_NAMESPACE_SYMBOL, key);
+    }
+
+    public getContext(namespace: symbol | string, key: string): any {
+
+        const contextMap = this._getNamespaceMap(namespace);
+        return contextMap.get(key);
+    }
+
+    private _getNamespaceMap(namespace: symbol | string): Map<string, any> {
+
+        const namespaceSymbol: symbol = this._getNamespaceSymbol(namespace);
+
+        if (!this._namespaces.has(namespaceSymbol)) {
+            this._namespaces.set(namespaceSymbol, new Map());
+        }
+        return this._namespaces.get(namespaceSymbol) as Map<string, any>;
+    }
+
+    private _getNamespaceSymbol(namespace: symbol | string): symbol {
+
+        if (typeof namespace === "symbol") {
+            return namespace;
+        }
+
+        return Symbol.for(namespace);
+    }
 }
