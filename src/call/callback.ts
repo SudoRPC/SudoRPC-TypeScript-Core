5db045098d7accea999cf3871db268ccf5e16780
wmxpy <wm@mengw.io>
2022-06-27 22:28:59 -0500

    add call manger

--- src/call/callback.ts
+++ src/call/callback.ts
@@ -4,6 +4,40 @@
  * @description Callback
  */
 
-export class SudoRPCCallCallback {
+import { SudoRPCReturnV1ErrorItem } from "../structure/return";
 
+export class SudoRPCCallCallback<SuccessResult, FailResult> {
+
+    public static create<SuccessResult, FailResult>(
+        resolver: (result: SuccessResult) => void,
+        rejecter: (errors: Array<SudoRPCReturnV1ErrorItem<FailResult>>) => void,
+    ): SudoRPCCallCallback<SuccessResult, FailResult> {
+
+        return new SudoRPCCallCallback<SuccessResult, FailResult>(
+            resolver,
+            rejecter,
+        );
+    }
+
+    private readonly _resolver: (result: SuccessResult) => void;
+    private readonly _rejecter: (errors: Array<SudoRPCReturnV1ErrorItem<FailResult>>) => void;
+
+    private constructor(
+        resolver: (result: SuccessResult) => void,
+        rejecter: (errors: Array<SudoRPCReturnV1ErrorItem<FailResult>>) => void,
+    ) {
+
+        this._resolver = resolver;
+        this._rejecter = rejecter;
+    }
+
+    public resolve(result: SuccessResult): void {
+
+        this._resolver(result);
+    }
+
+    public reject(errors: Array<SudoRPCReturnV1ErrorItem<FailResult>>): void {
+
+        this._rejecter(errors);
+    }
 }
--- src/call/manager.ts
+++ src/call/manager.ts
@@ -4,6 +4,67 @@
  * @description Manager
  */
 
-export class SudoRPCCallManager {
+import { SudoRPCReturnV1ErrorItem } from "../structure/return";
+import { SudoRPCCallCallback } from "./callback";
 
+export class SudoRPCCallManager<Metadata, Payload, SuccessResult, FailResult> {
+
+    private readonly _callbacks: Map<string, SudoRPCCallCallback<SuccessResult, FailResult>>;
+
+    private constructor() {
+
+        this._callbacks = new Map();
+    }
+
+    public makeCall(
+        resource: string,
+        metadata: Metadata,
+        payload: Payload,
+    ): Promise<SuccessResult> {
+
+        return new Promise<SuccessResult>((
+            resolve: (result: SuccessResult) => void,
+            reject: (errors: Array<SudoRPCReturnV1ErrorItem<FailResult>>) => void,
+        ) => {
+
+            const callback: SudoRPCCallCallback<SuccessResult, FailResult> =
+                SudoRPCCallCallback.create(resolve, reject);
+
+            this._callbacks.set(resource, callback);
+        });
+    }
+
+    public resolveCall(
+        identifier: string,
+        result: SuccessResult,
+    ): void {
+
+        if (!this._callbacks.has(identifier)) {
+            throw new Error(`Cannot find callback with identifier: ${identifier}`);
+        }
+
+        const callback: SudoRPCCallCallback<SuccessResult, FailResult> =
+            this._callbacks.get(identifier) as SudoRPCCallCallback<SuccessResult, FailResult>;
+
+        if (callback) {
+            callback.resolve(result);
+        }
+    }
+
+    public rejectCall(
+        identifier: string,
+        errors: Array<SudoRPCReturnV1ErrorItem<FailResult>>,
+    ): void {
+
+        if (!this._callbacks.has(identifier)) {
+            throw new Error(`Cannot find callback with identifier: ${identifier}`);
+        }
+
+        const callback: SudoRPCCallCallback<SuccessResult, FailResult> =
+            this._callbacks.get(identifier) as SudoRPCCallCallback<SuccessResult, FailResult>;
+
+        if (callback) {
+            callback.reject(errors);
+        }
+    }
 }
